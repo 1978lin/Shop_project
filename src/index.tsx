6be2b8860ef3d41461abe40a160e5012a77e32d3
Igor Liventsov <i.liventsov@gmail.com>
2022-08-10 18:45:49 +0300

    react day-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const h1 = <h1>Hello world</h1>
+
+function App() {
+  return (
+    <div>
+      {h1}
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eius velit saepe, modi minus asperiores laboriosam quisquam sapiente quidem eligendi culpa harum aut fugit adipisci sunt placeat quod. Eveniet, rem.
+
+      </p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  document.getElementById('root') as HTMLElement);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App/>);
+
+root.render(h1);
+
