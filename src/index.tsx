b36c064e9fba243eebdb39e4a2873bdc1e0e9ae4
Igor Liventsov <i.liventsov@gmail.com>
2022-09-13 11:54:18 +0300

    add test App

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const h1 = <h1>Hello world!</h1>;
+
+function App() {
+  return (
+    <div>
+      {h1}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus omnis
+        mollitia officia quia possimus corporis, fuga est inventore quas. Est
+        sed aperiam accusamus. Fuga vel harum, error quibusdam voluptatibus
+        eaque?
+      </p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render("Hello world!");
+root.render(<App />);
