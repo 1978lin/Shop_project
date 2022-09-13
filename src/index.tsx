4fa63cbe9d7f0b7e15db440c525dec8c54835f6c
Igor Liventsov <i.liventsov@gmail.com>
2022-09-13 10:44:10 +0300

    clean CRA

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-
-const h1 = <h1>Hello world</h1>
-
-function App() {
-  return (
-    <div>
-      {h1}
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eius velit saepe, modi minus asperiores laboriosam quisquam sapiente quidem eligendi culpa harum aut fugit adipisci sunt placeat quod. Eveniet, rem.
-
-      </p>
-    </div>
-  )
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement);
+  document.getElementById("root") as HTMLElement
+);
 
-root.render(<App/>);
-
-root.render(h1);
-
+root.render("Hello world!");
