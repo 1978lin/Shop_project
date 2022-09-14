5d48f35eed80cea97ce6cb0fe91d88133ed1bd5b
Igor Liventsov <i.liventsov@gmail.com>
2022-09-14 11:26:55 +0300

    add MUI and MUI SVG icons

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,29 +1,9 @@
 import React from 'react'
 
-type Props = {
-    title: string
-}
-
-const AppList = () => {
-    return (
-        <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-        </ul>
-    )
-}
-
-const AppHeader = (props: Props) => {
-    return <h1>Hello {props.title}</h1>
-}
-
 const App = () => {
     return (
         <>
-            <AppHeader title="JSX" />
-            <AppHeader title="React" />
-            <AppList />
+            <h1>Hello world!</h1>
         </>
     )
 }
